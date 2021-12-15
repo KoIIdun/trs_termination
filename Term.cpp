20954ca73eb4c3936c3d732a8236ad61fc24d6d4
Stephan <styopa.plaunov@gmail.com>
2021-12-15 06:32:43 +0300

    +term

--- Term.cpp
+++ Term.cpp
@@ -3,5 +3,48 @@
 //
 
 #include "Term.h"
+#include <iostream>
 
-Term::Term(const string &term) : term(term) {}
+bool Term::correct = true;
+
+map<string, int> Term::constructorsMap = map<string, int>({});
+
+Term::Term(string_view strterm){
+    term = strterm;
+    int constructorIndex = term.find_first_of('(');
+    vector<Term> arguments(0);
+    if (constructorIndex != -1) {
+        constructor = term.substr(0, term.find_first_of('('));
+
+        int bracketcounter = 0;
+        int lastvirg = constructorIndex;
+        for (int i = constructorIndex + 1; i < term.length(); i++) {
+            if (term[i] == '(') {
+                bracketcounter++;
+            } else if (term[i] == ')') {
+                bracketcounter--;
+            }
+            if ((term[i] == ',' && bracketcounter == 0) || (i == term.length() - 1 && bracketcounter == -1)){
+                arguments.push_back(Term(term.substr(lastvirg + 1, i - lastvirg - 1)));
+                lastvirg = i;
+            }
+        }
+    } else {
+        constructor = strterm;
+        isVariable = true;
+    }
+    if (constructorsMap.count(constructor) != 0) {
+        if (constructorsMap[constructor] != arguments.size()) {
+            correct = false;
+        }
+    } else {
+        constructorsMap[constructor] = arguments.size();
+    }
+    //cout << constructor << " " << term << endl;
+}
+
+Term::Term() {}
+
+bool Term::isCorrect() {
+    return correct;
+}
--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ using namespace std;
 int main() {
     string data;
     Trs trs("test_dickson1.trs");
+    Term term("adas(f(x),f(z(x),y))");
+    cout << term.isCorrect();
     return 0;
 }
